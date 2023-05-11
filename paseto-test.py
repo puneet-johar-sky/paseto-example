e6a970c5a96a8fb7444aecd3c3a5195260be835a
Puneet Johar <puneet.johar@sky.uk>
2023-05-11 17:03:52 +0100

    added negative test case

--- paseto-test.py
+++ paseto-test.py
@@ -16,21 +16,30 @@ with open("./private_key.pem") as key_file:
     private_key = Key.new(3, "public", key_file.read())
 token = pyseto.encode(
     private_key,
-    payload=json.dumps(data),
-    footer=b'{"keyid":"001"}',  # Optional
-    implicit_assertion=b"xyz",  # Optional
+    payload=json.dumps(data)
+    # footer=b'{"keyid":"001"}',  # Optional
+    # implicit_assertion=b"xyz",  # Optional
 )
-print(token)
+# token is example of the header that one would receive in the request 
+print(f"X-Sky-OTT-CapabilitiesOverride:{1}\n" , token)
+#initiliase the decoding via the public key
 with open("./public_key.pem") as key_file:
     public_key = Key.new(3, "public", key_file.read())
-decoded = pyseto.decode(public_key, token,implicit_assertion=b"xyz")
+decoded = pyseto.decode(public_key, token)
 data_test = json.loads( decoded.payload)
-print("--------")
+print("--------Good Header Example-------")
 print(data_test)
 
 assert (data == data_test)
 
-assert decoded.footer == b'{"keyid":"001"}'
+# assert decoded.footer == b'{"keyid":"001"}'
 assert decoded.version == "v3"
 assert decoded.purpose == "public"
 
+#example bad header token
+print("--------BAD Header Example-------")
+try: 
+    bad_header = "v3.public.vB7daJlQOL5sY8mQa_FWb6ZYbkNi8yeRqI-DCFNEPTYEu7ItQHMMM5jzD_fw-G7l-AXJRBj3E9jxx9-JS5eG436WGUn03zYp2nuV3PVqppEyRP9LoZ1TTBREhR182NRcNYqUkM8FfazWegWcLc1gSzFXx0Kge4U7XHtAlliTrR8p09hH6qVpqAsgMdp00ao66JX_mxlEjkL3y784CoAK-gyy_ZZ1WzAvYAjQApl859RxnB9uLMpb-VURmetmrw9sC_Iw27to46ulTcMxx_KoSBem9eSG5M4bvNQC5YFeDLIM2HXDf35YIo50.eyJraWQiOiAiMTIzNDUifQ"
+    bad_decode = pyseto.decode(public_key, bad_header)
+except Exception as e: 
+    print(f"Verification failed {1}",e)
