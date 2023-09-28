dce9e75011132df99dc094fd1e6ce97e8be2f5f4
Puneet Johar <puneet.johar@sky.uk>
2023-09-28 17:20:31 +0100

    added jwt signing example

---
+++ jwt-example.py
@@ -0,0 +1,13 @@
+import jwt
+
+with open("./private_key.pem",mode="rb") as key_file:
+    private_key =  key_file.read()
+
+with open("./jwtRS256.key.pub",mode="rb") as key_file:
+    public_key =  key_file.read()
+
+encoded = jwt.encode({"some": "payload"}, private_key, algorithm="RS256")
+print(encoded)
+
+decoded = jwt.decode(encoded, public_key, algorithms=["RS256"])
+print(decoded)
